eafc16d7a5873d9237cfa807be311678e0408d42
CalvinLezes <ludiziy@mail.ru>
2023-06-03 01:26:54 +0700

    update analiser

--- article_analizer.py
+++ article_analizer.py
@@ -2,6 +2,7 @@ import nltk
 from nltk.corpus import stopwords
 import logging
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
 
 class ArticleAnalizer:
